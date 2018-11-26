e510cc2c2a12666b2fc6962718e9d469afe177df
Andrew Miracle <me@andrewmiracle.com>
2018-11-26 11:02:13 +0200

    working with enum

--- hello/README.md
+++ hello/README.md
@@ -14,6 +14,26 @@ log(message);
 
 ```js
 
+// types in ts
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1,2,3]; //only array of numbers
+let f: any[] = [1, true, 'a', false]; 
+
+
+// working with enums types in TS
+// instead of declaring multiple related variables 
+const ColorRed = 0;
+const ColorGreen = 1;
+const ColorBlue = 2;
+
+// using enum
+enum Coolor {Red, Green, Blue};
+let backgroundColor = Coolor.Blue;
+
+
 
 ```
 
--- hello/main.js
+++ hello/main.js
@@ -1,6 +1,21 @@
-function log(message) {
-    console.log(message);
-}
-// call message
-var message = "Hello World";
-log(message);
+// types in ts
+var a;
+var b;
+var c;
+var d;
+var e = [1, 2, 3]; //only array of numbers
+var f = [1, true, 'a', false];
+// working with enums types in TS
+// instead of declaring multiple related variables 
+var ColorRed = 0;
+var ColorGreen = 1;
+var ColorBlue = 2;
+// using enum
+var Coolor;
+(function (Coolor) {
+    Coolor[Coolor["Red"] = 0] = "Red";
+    Coolor[Coolor["Green"] = 1] = "Green";
+    Coolor[Coolor["Blue"] = 2] = "Blue";
+})(Coolor || (Coolor = {}));
+;
+var backgroundColor = Coolor.Blue;
