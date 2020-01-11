6697fffa1987ba51ff95400c9be47236f008499e
Andrew Miracle <me@andrewmiracle.com>
2020-01-11 09:23:24 +0000

    write all functions for todo

--- app-tsx/src/App.tsx
+++ app-tsx/src/App.tsx
@@ -9,17 +9,53 @@ interface TodoInterface {
 
 const App: React.FC = () => {
   const [value, setValue] = useState<string>('');
+  const [todos, setTodos] = useState<TodoInterface[]>([]);
 
   // Submit event to state for form
   let handleSubmit = (e: React.FormEvent<HTMLFormElement>) : void => {
+    // now create a todo object with the value from useState and a boolean for complete
+    // value is defined from handleChange event
+    const todoDictionary: TodoInterface = {text:value, complete:false};
     e.preventDefault()
+    addTodo(todoDictionary)
     setValue('');
   }
 
   // Change event for input form
   let handleChange = (e: ChangeEvent<HTMLInputElement>): void => setValue(e.target.value);
 
-  // Event to Complete Todo
+  /**
+   * Event handler to delete a Todolist from the todos in app state
+   */
+  let deleteTodo = (index : number) : void => {
+    // Method One
+    // const listItems : TodoInterface[] = [...todos]
+    // listItems.splice(index, 1);
+    // setTodos(listItems)
+    
+    // Method Two
+    const value = todos[index]
+    const todoItems : TodoInterface[] = todos.filter( item => item !== value)
+    // debugger
+    setTodos(todoItems)
+  }
+
+  // Event to Add Todo
+  let addTodo = (todoList : TodoInterface) : void => {
+    const newList : TodoInterface[] = [...todos, todoList]
+    // call setTodo from usestate
+    setTodos(newList)
+  }
+
+  /**
+   *Event to Complete Todo : We want to get the current index of the clicked element and 
+   set the complete todo boolean to true or false
+   */
+  let completeTodo = (index: number) => {
+    const todoList: TodoInterface[] = [...todos];
+    todoList[index].complete = !todoList[index].complete;
+    setTodos(todoList)
+  }
 
 
   return (
@@ -31,6 +67,17 @@ const App: React.FC = () => {
         <button type="submit">Add Todo</button>
         </form>
       </header>
+
+      {todos.map((todo : TodoInterface, index) => (
+          <section key={index} className="todoSection">
+            <div className="todoElement">
+            <input type="checkbox" onChange={() => completeTodo(index)} />
+            <p style={{textDecoration: todo.complete ? 'line-through' : ''}}>{todo.text}</p>
+            </div>
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </section>   
+      ))}
+
     </div>
   );
 }
